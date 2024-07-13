185c7d2874ca1599942af880cb26634644688095
7006 <22399553+7006@users.noreply.github.com>
2024-07-13 14:48:07 +0000

    Require presence of ONLY_FOOLHARDY_SCALES_DATABASE_DSN env variable

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<html>
-  <head>
-    <title>PHP Test</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+<?php
 
-</html>
+require __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$dotenv->required(['ONLY_FOOLHARDY_SCALES_DATABASE_DSN'])->notEmpty();
