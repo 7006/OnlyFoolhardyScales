9cea46f4a7f21ac282d7e2b7ed9e89f08f3aa372
oromadin <osromadin@gmail.com>
2024-07-13 20:45:21 +0000

    Add function getAuthorOf1984

--- src/App.php
+++ src/App.php
@@ -5,12 +5,26 @@ namespace OnlyFoolhardyScales;
 class App {
   public static function run() {
     $db = static::db();
-    echo $db->query('SELECT version()')->fetch()['version'];
+    echo static::getDbVersion();
+    echo static::getAuthorOf1984();
   }
 
   private static function db() {
     $dsn = $_ENV['ONLY_FOOLHARDY_SCALES_DATABASE_DSN'];
     $db = new \PDO($dsn);
     return $db;
+  }
+
+  public static function getDbVersion () {
+    $db = static::db();
+    return $db->query('SELECT version()')->fetch()['version'];
+  }
+  
+  public static function getAuthorOf1984 () {
+    $db = static::db();
+    $statement = $db->query("SELECT author FROM books_to_read WHERE title = '1984'");
+    $statement->execute();
+    $output = $statement->fetch();
+    return $output['author'];
   }
 }
